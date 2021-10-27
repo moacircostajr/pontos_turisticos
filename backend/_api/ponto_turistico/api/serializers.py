c0e011384ad1d7d7f82d6af7c4f344a925a6491a
Moacir Costa <moacircostajr@gmail.com>
2021-10-27 17:25:24 -0300

    feat: nested relationships

--- backend/_api/ponto_turistico/api/serializers.py
+++ backend/_api/ponto_turistico/api/serializers.py
@@ -2,11 +2,26 @@ from rest_framework.serializers import ModelSerializer
 from ponto_turistico.models import PontoTuristico
 
 class PontoTuristicoSerializer(ModelSerializer):
-    class Meta:
-        model = PontoTuristico
-        fields = ['id', 'nome', 'descricao', 'foto']
+    atracoes = AtracaoSerializer(many=True, read_only=True)
+    endereco = EnderecoSerializer(read_only=True)
+    descricao_completa = SerializerMethodField()
 
-class PontoTuristicoFullSerializer(ModelSerializer):
     class Meta:
         model = PontoTuristico
-        fields = ['id', 'nome', 'descricao', 'aprovado', 'atracao', 'comentarios', 'avaliacoes', 'endereco', 'foto']
+        fields = ['id', 'nome', 'descricao', 'aprovado', 'foto', 'atracoes', 'comentarios', 'avaliacoes', 'endereco', 'descricao_completa', 'descricao_completa2']
+        read_only_fields=['comentarios', 'avaliacoes']
+
+    def cria_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def create(self, validated_data):
+        atracoes = validated_data['atracoes']
+        del validated_data['atracoes']
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.cria_atracoes(atracoes, ponto)
+        return ponto
+
+    def get_descricao_completa(self, obj):
+        return '%s - %s' % (obj.nome, obj.descricao)
